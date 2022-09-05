9c90760d95ce002ff7643610b1a9380ab7a11dae
Ian-Barton <bobbarbid.1@gmail.com>
2022-09-04 22:16:46 -0500

    My Part Is Done
    I added the fields for the user to input and submit. I then added the scripting. It removes the space between the two words and then lower cases it. It then compares it to a reversed version to see if they are the same.

--- js/site.js
+++ js/site.js
@@ -1,3 +1,31 @@
-function HelloWorld() {
-    alert("Hello World");
+//Get the String input
+//Controller function
+function getValues() {
+    document.getElementById("alert").classList.add("invisible");
+    let userString = document.getElementById("userString").value;
+    userString = userString.replace(" ", "");
+    userString = userString.toLocaleLowerCase(userString);
+    reverseString(userString);
+}
+//Reverse the String
+//Logic Function
+function reverseString(userString) {
+    let revString = [];
+
+    for (let i = userString.length - 1; i >=0; i--) {
+        revString += userString[i];
+    }
+    displayTacocat(userString, revString)
+}
+//Display the message
+//View Function
+function displayTacocat(userString, revString) {
+    if (userString == revString) {
+        document.getElementById("msg").innerHTML = `Your palindrome is: ${userString}`;
+        document.getElementById("alert").classList.remove("invisible");
+    }
+    else {
+        document.getElementById("msg").innerHTML = `You don't have a palindrome`;
+        document.getElementById("alert").classList.remove("invisible");
+    }
 }
